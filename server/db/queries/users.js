d3116369a30df2b1925103f771e4f21ed11ea8e7
RinatTr <rinatreg@gmail.com>
2019-10-02 17:53:00 -0400

    add getAUser query

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -12,6 +12,19 @@ const getAllUsers = (req, res, next) => {
     .catch(err => next(err))
 }
 
+const getUser = (req, res, next) => {
+  let userId = req.params.userId;
+  db.one("SELECT * FROM users WHERE id = $1", [userId])
+    .then(data => {
+      res.status(200).json({
+        status: "success",
+        message: "got user",
+        user: data
+      })
+    })
+    .catch(err => next(err))
+}
+
 const createUser = (req, res, next) => {
   let username = req.params.username;
   db.none("INSERT INTO users(username) VALUES($1)", [username])
@@ -26,5 +39,6 @@ const createUser = (req, res, next) => {
 
 module.exports = {
   getAllUsers,
+  getUser,
   createUser
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, createUser } = require('../db/queries/users.js');
+const { getAllUsers, createUser, getUser } = require('../db/queries/users.js');
 
+router.get('/:userId', getUser)
 router.get('/', getAllUsers)
 router.post('/:username', createUser)
 
