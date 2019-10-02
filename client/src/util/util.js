4ad19881dfcbda1b6777b0f648936fe8c86034b0
RinatTr <rinatreg@gmail.com>
2019-10-02 19:20:56 -0400

    add api calls

--- client/src/util/util.js
+++ client/src/util/util.js
@@ -1,5 +1,17 @@
 const axios = require('axios');
 
-// API calls
-axios.get()
-
+// API calls:
+// Messages
+export const getAllMessages = () => { 
+    return axios.get('/api/messages') 
+};
+export const createMessage = (req_body) => {
+    return axios.post('/api/messages', req_body)
+};
+// Users
+export const getAllUsers = () => {
+    return axios.get('/api/users')
+};
+export const createUser = (username) => {
+    return axios.post(`/api/users/${username}`)
+};
