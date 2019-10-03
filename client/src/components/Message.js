adfe61341ed00f1dc568f6340591b97cda22ef9a
RinatTr <rinatreg@gmail.com>
2019-10-02 20:13:06 -0400

    init timeago

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import TimeAgo from 'react-timeago';
 
 const Message = ({body, username, timestamp}) => {
     let avatar_url = `https://i.pravatar.cc/50?u=${username}`
- return <li>
-            <img src={avatar_url}/> {username}: {body}
-        </li>
+    return  <li>
+                <img src={avatar_url}/> {username}: {body}
+                <TimeAgo date={timestamp} />
+            </li>
 }
 
 export default Message;
