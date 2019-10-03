5a0625d319d8ede64374fe656368d8c41b2dae23
RinatTr <rinatreg@gmail.com>
2019-10-03 10:35:03 -0400

    init form

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Form from './Form.js';
 import Message from './Message.js';
 import * as Util from '../util/util.js';
 
@@ -7,27 +6,40 @@ class Main extends Component {
     constructor() {
         super()
         this.state = {
-            messages: [],
+            msgs: [],
             username: "",
-            avatar: ""
+            prompt: ""
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     async componentDidMount() {
         try {
             let { data } = await Util.getAllMessages()
-            this.setState({messages: data.messages});
+            this.setState({msgs: data.messages});
         } catch(e) {
             console.log(e, "could not get messages from server")
         }    
     }
+    handleChange = (e) => {
+        this.setState({prompt: e.target.value})
+    }
+    async handleSubmit(e) {
+        e.preventDefault();
+        //if no username, set username. else, post message
+        if (!this.state.username) {
+            Util.createUser(e.target.value);
+        } else {
+            
+        }
+    }
 
     render() {
-        let { messages } = this.state; 
+        let { msgs, prompt, username } = this.state; 
         return (
             <div className="main-container">
                 <div className="messages-container">
                     Better Chat
-                    {messages.map((msg, i) => {
+                    {msgs.map((msg, i) => {
                         return <Message 
                             key={i}
                             body={msg.body} 
@@ -38,7 +50,14 @@ class Main extends Component {
                     }
                 </div>
                 <div className="form-container">
-
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            name="prompt" 
+                            onChange={this.handleChange} 
+                            value={prompt} 
+                            placeholder={username ? "type message" : "enter username"}/>
+                        <button type="submit">click</button>
+                    </form>
                 </div>
             </div>
         )
