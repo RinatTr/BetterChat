a866281c63924cdd8957e5438950e0c62dfb3981
RinatTr <rinatreg@gmail.com>
2019-10-04 23:00:15 -0400

    add non-unique test

---
+++ client/src/components/Main.test.js
@@ -0,0 +1,22 @@
+//When comp rerenders, need to be booleans. 
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Main from './Main';
+import axios from 'axios';
+
+// Jest does not use proxy for API calls
+const PROXY = "http://localhost:3100";
+
+// test('Main snapshot test', () => {
+//   const component = renderer.create(<Main />);
+//   const tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
+test('non-unique username request fails with expected error code', () => {
+    const EXISTING_USERNAME = "wolverine";
+    return axios.post(`${PROXY}/api/users/${EXISTING_USERNAME}`).catch(e => {
+        let err_code = e.response.data.error.code; 
+        expect(err_code).toBe('23505');
+    });
+  });
